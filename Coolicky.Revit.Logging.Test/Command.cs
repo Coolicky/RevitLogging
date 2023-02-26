da7408d54ea93f9f8ad4d5fcc383d5c3c663df94
coolicky <piotr.kulicki@gmail.com>
2023-02-26 18:33:42 +0000

    updated with working test project.

--- Coolicky.Revit.Logging.Test/Command.cs
+++ Coolicky.Revit.Logging.Test/Command.cs
@@ -1,12 +1,33 @@
+using System;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Revit.DependencyInjection.Unity.Commands;
+using Revit.DependencyInjection.Unity.RibbonCommands.Attributes;
+using Serilog;
+using Unity;
 
 namespace Coolicky.Revit.Logging.Test
 {
-    public class Command : IExternalCommand
+    [Regeneration(RegenerationOption.Manual)]
+    [Transaction(TransactionMode.Manual)]
+    [RibbonPushButton(
+        PanelName = "Logging Test",
+        FirstLine = "Test"
+    )]
+    public class Command : RevitAppCommand<App>
     {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        public override Result Execute(IUnityContainer container, ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            Log.Logger.Activity(new Activity
+            {
+                Name = "Test",
+                Description = "Description",
+                Details = new[] { "First", "Second" }
+            });
+            
+            Log.Logger.Error(new Exception("Whatever Men"), "Error Occured in testing");
+
             return Result.Succeeded;
         }
     }
