d74da5c6a18aa2b37370f4c08e47c0a27cf96b5b
coolicky <piotr.kulicki@gmail.com>
2023-02-26 11:25:10 +0000

    basic serilog setup

--- Coolicky.Revit.Logging/Activity.cs
+++ Coolicky.Revit.Logging/Activity.cs
@@ -2,6 +2,8 @@ namespace Coolicky.Revit.Logging
 {
     public class Activity
     {
-        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Details { get; set; }
     }
 }
--- Coolicky.Revit.Logging/RevitSerilogExtension.cs
+++ Coolicky.Revit.Logging/RevitSerilogExtension.cs
@@ -1,7 +1,40 @@
+using System;
+using Autodesk.Revit.UI;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+
 namespace Coolicky.Revit.Logging
 {
-    public class RevitSerilogExtension
+    public static class RevitSerilogExtension
     {
-        
+        public static LoggerConfiguration CreateRevitConfiguration(this LoggerConfiguration config,
+            UIControlledApplication app, string appName, string appVersion)
+        {
+            return config.Enrich.WithExceptionDetails()
+                .Enrich.WithProperty("machineName", Environment.MachineName)
+                .Enrich.WithProperty("userName", Environment.UserName)
+                .Enrich.WithProperty("domain", Environment.UserDomainName)
+                .Enrich.WithProperty("operatingSystem", Environment.OSVersion)
+                .Enrich.WithProperty("revitVersionName", app.ControlledApplication.VersionName)
+                .Enrich.WithProperty("revitVersionNo", app.ControlledApplication.VersionNumber)
+                .Enrich.WithProperty("revitSubVersionNo", app.ControlledApplication.SubVersionNumber)
+                .Enrich.WithProperty("revitVersionBuild", app.ControlledApplication.VersionBuild)
+                .Enrich.WithProperty("revitType", app.ControlledApplication.Product.ToString())
+                .Enrich.WithProperty("revitLanguage", app.ControlledApplication.Language.ToString())
+                .Enrich.WithProperty("app", appName)
+                .Enrich.WithProperty("version", appVersion);
+        }
+
+        public static void Error(this ILogger logger, Exception exception, Activity activity)
+        {
+            logger.Error(exception, "Error Occurred for Activity {@Activity}", activity);
+        }
+
+        public static void Activity(this ILogger logger, Activity activity,
+            LogEventLevel level = LogEventLevel.Information)
+        {
+            logger?.Write(level, "Activity: {@Activity}", activity);
+        }
     }
 }
